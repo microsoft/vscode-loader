21a9560a595e23c00a3160be2c4779ba057301df
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-09-09 15:45:42 +0200

    Remove unused fallback code path

--- src/css.ts
+++ src/css.ts
@@ -64,7 +64,7 @@ module CSSLoaderPlugin {
 		public _insertLinkNode(linkNode: HTMLLinkElement): void {
 			this._pendingLoads++;
 			var head = document.head || document.getElementsByTagName('head')[0];
-			var other: HTMLCollectionOf<HTMLElement> = head.getElementsByTagName('link') || head.getElementsByTagName('script');
+			var other: HTMLCollectionOf<HTMLElement> = head.getElementsByTagName('link');
 			if (other.length > 0) {
 				head.insertBefore(linkNode, other[other.length - 1]);
 			} else {
